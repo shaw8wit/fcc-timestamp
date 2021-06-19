8d767ef688b705c4b3a168207c23699c5f387759
shaw8wit <shaw8wit@gmail.com>
2021-06-19 19:47:36 +0530

    add timestamp microservice

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+
+app.get("/api/:date?", (req, res) => {
+  let unixPattern = /\d{13}/;
+  let date = null;
+  if (!req.params.date || unixPattern.test(req.params.date) || (new Date(req.params.date) != "Invalid Date")) {
+    if(!req.params.date) {
+      date = new Date();
+    } else if (unixPattern.test(req.params.date)) {
+      date = new Date(Number(req.params.date));
+    } else {
+      date = new Date(req.params.date);
+    }
+  } else {
+    res.json({"error": "Invalid Date"});
+  }
+  res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
